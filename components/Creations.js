24b019a13f90d9b70a719275583d0d00b9519d33
1391819 <roberto.nacu@gmail.com>
2022-09-08 15:41:18 +0100

    added tic-tac-toe project, create general thumbnail for projects with no images

--- components/Creations.js
+++ components/Creations.js
@@ -4,7 +4,7 @@ import CreationGridItem from "./CreationGridItem.js";
 // thumbnails
 import faceRecognitionThumbnail from "../public/images/creations/face-recognition/analysis.jpg";
 import japaneseLanguageRenshuuThumbnail from "../public/images/creations/japanese-language-renshuu/4.4.jpg";
-
+import generalProjectThumbnail from "../public/images/creations/generalProjectThumbnail.jpg";
 const Creations = () => {
   return (
     <SimpleGrid columns={[1, null, 2, null, null]} gap={6} pt={4}>
@@ -59,6 +59,44 @@ const Creations = () => {
               colorScheme="blue"
             >
               Python
+            </Badge>
+          </Box>
+        </CreationGridItem>
+      </Box>
+      <Box>
+        <CreationGridItem
+          title="Tic Tac Toe"
+          thumbnail={generalProjectThumbnail}
+          link="https://github.com/1391819/tic-tac-toe"
+        >
+          <Box pb={1}>
+            <Badge
+              className="stack-badge"
+              fontSize={["xs", null, null, null, null, null]}
+              colorScheme="blue"
+            >
+              C++
+            </Badge>
+            <Badge
+              className="stack-badge"
+              fontSize={["xs", null, null, null, null, null]}
+              colorScheme="purple"
+            >
+              Linux
+            </Badge>
+            <Badge
+              className="stack-badge"
+              fontSize={["xs", null, null, null, null, null]}
+              colorScheme="red"
+            >
+              Minimax
+            </Badge>
+            <Badge
+              className="stack-badge"
+              fontSize={["xs", null, null, null, null, null]}
+              colorScheme="orange"
+            >
+              TCP Sockets
             </Badge>
           </Box>
         </CreationGridItem>
