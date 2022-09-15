047295cc557ae99f43135973461b697d1962b47e
1391819 <roberto.nacu@gmail.com>
2022-09-15 11:20:15 +0100

    added university-social-media project

--- components/Creations.js
+++ components/Creations.js
@@ -6,6 +6,7 @@ import faceRecognitionThumbnail from "../public/images/creations/face-recognitio
 import japaneseLanguageRenshuuThumbnail from "../public/images/creations/japanese-language-renshuu/japanese_language_renshuu_showcase.png";
 import interactiveLowPolyEnvironmentThumbnail from "../public/images/creations/interactive-low-poly-environment/interactive_low_poly_environment_showcase.png";
 import maSeekThumbnail from "../public/images/creations/MA-seek/ma_seek_showcase.png"
+import uniSocialMediaThumbnail from "../public/images/creations/social-media-app/uni_social_media_showcase.png"
 import generalProjectThumbnail from "../public/images/creations/generalProjectThumbnail.png";
 
 const Creations = () => {
@@ -176,6 +177,30 @@ const Creations = () => {
               colorScheme="orange"
             >
               DRQN
+            </Badge>
+          </Box>
+        </CreationGridItem>
+      </Box>
+      <Box>
+        <CreationGridItem
+          title="University Social Media"
+          thumbnail={uniSocialMediaThumbnail}
+          link="https://github.com/1391819/social_media_design"
+        >
+          <Box pb={1}>
+            <Badge
+              className="stack-badge"
+              fontSize={["xs", null, null, null, null, null]}
+              colorScheme="blue"
+            >
+              HCI
+            </Badge>
+            <Badge
+              className="stack-badge"
+              fontSize={["xs", null, null, null, null, null]}
+              colorScheme="purple"
+            >
+              UX/UI
             </Badge>
           </Box>
         </CreationGridItem>
