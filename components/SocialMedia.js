f23410f96486a04fab107c6fd1c41f0e7f8c9725
1391819 <roberto.nacu@gmail.com>
2022-09-20 01:03:41 +0100

    removed instagram from social media, added kaggle, changed icon type

--- components/SocialMedia.js
+++ components/SocialMedia.js
@@ -1,22 +1,23 @@
-import { useColorMode, Stack, Link, Button, Icon } from "@chakra-ui/react";
-import { IoLogoInstagram, IoLogoGithub, IoLogoLinkedin } from "react-icons/io5";
+import {useColorMode, Stack, Link, Button, Icon} from "@chakra-ui/react";
+import {IoLogoInstagram, IoLogoGithub, IoLogoLinkedin} from "react-icons/io5";
+import {FaGithub, FaKaggle, FaLinkedin} from "react-icons/fa";
 
 const SocialMedia = () => {
-  const { colorMode } = useColorMode();
+  const {colorMode} = useColorMode();
 
   const socialIconColor = {
     light: "red.400",
-    dark: "red.400",
+    dark: "red.400"
   };
 
   const socialBgColor = {
     light: "whiteAlpha",
-    dark: "blackAlpha",
+    dark: "blackAlpha"
   };
 
   const socialHoverBg = {
     light: "teal.500",
-    dark: "teal.300",
+    dark: "teal.300"
   };
 
   return (
@@ -24,7 +25,7 @@ const SocialMedia = () => {
       <Link href="https://github.com/1391819" target="_blank">
         <Button
           aria-label="Open Github"
-          leftIcon={<Icon mr={-2} as={IoLogoGithub} />}
+          leftIcon={<Icon mr={-2} as={FaGithub} />}
           borderRadius={"5px"}
           p={0}
           color={socialIconColor[colorMode]}
@@ -32,14 +33,14 @@ const SocialMedia = () => {
           fontSize={["lg", "md", null, null, null, null]}
           _hover={{
             color: "#e5e5e5",
-            bg: socialHoverBg[colorMode],
+            bg: socialHoverBg[colorMode]
           }}
         ></Button>
       </Link>
-      <Link href="https://www.instagram.com/roberto.nacu/" target="_blank">
+      <Link href="https://www.kaggle.com/robertonacu" target="_blank">
         <Button
           aria-label="Open Instagram"
-          leftIcon={<Icon mr={-2} as={IoLogoInstagram} />}
+          leftIcon={<Icon mr={-2} as={FaKaggle} />}
           borderRadius={"5px"}
           p={0}
           ml={2}
@@ -48,14 +49,14 @@ const SocialMedia = () => {
           fontSize={["lg", "md", null, null, null, null]}
           _hover={{
             color: "#e5e5e5",
-            bg: socialHoverBg[colorMode],
+            bg: socialHoverBg[colorMode]
           }}
         ></Button>
       </Link>
       <Link href="https://www.linkedin.com/in/robertonacu/" target="_blank">
         <Button
           aria-label="Open Instagram"
-          leftIcon={<Icon mr={-2} as={IoLogoLinkedin} />}
+          leftIcon={<Icon mr={-2} as={FaLinkedin} />}
           borderRadius={"5px"}
           p={0}
           ml={2}
@@ -64,7 +65,7 @@ const SocialMedia = () => {
           fontSize={["lg", "md", null, null, null, null]}
           _hover={{
             color: "#e5e5e5",
-            bg: socialHoverBg[colorMode],
+            bg: socialHoverBg[colorMode]
           }}
         ></Button>
       </Link>
