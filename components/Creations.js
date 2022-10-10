bdfde2bd04a3486ca23cf5cf5de7856c38b130fc
1391819 <roberto.nacu@gmail.com>
2022-10-10 01:34:15 +0100

    removed tic tac toe project, added financial researcher projectin /creations

--- components/Creations.js
+++ components/Creations.js
@@ -1,17 +1,55 @@
-import { SimpleGrid, Box, Badge } from "@chakra-ui/react";
+import {SimpleGrid, Box, Badge} from "@chakra-ui/react";
 import CreationGridItem from "./CreationGridItem.js";
 
 // thumbnails
 import faceRecognitionThumbnail from "../public/images/creations/face-recognition/face_recognition_showcase.png";
 import japaneseLanguageRenshuuThumbnail from "../public/images/creations/japanese-language-renshuu/japanese_language_renshuu_showcase.png";
 import interactiveLowPolyEnvironmentThumbnail from "../public/images/creations/interactive-low-poly-environment/interactive_low_poly_environment_showcase.png";
-import maSeekThumbnail from "../public/images/creations/MA-seek/ma_seek_showcase.png"
-import uniSocialMediaThumbnail from "../public/images/creations/social-media-app/uni_social_media_showcase.png"
+import maSeekThumbnail from "../public/images/creations/MA-seek/ma_seek_showcase.png";
+import uniSocialMediaThumbnail from "../public/images/creations/social-media-app/uni_social_media_showcase.png";
 import generalProjectThumbnail from "../public/images/creations/generalProjectThumbnail.png";
 
 const Creations = () => {
   return (
     <SimpleGrid columns={[1, null, 2, null, null]} gap={6} pt={4}>
+      <Box>
+        <CreationGridItem
+          title="Financial Researcher"
+          thumbnail={generalProjectThumbnail}
+          link="https://huggingface.co/spaces/r1391819/financial-researcher"
+        >
+          <Box pb={1}>
+            <Badge
+              className="stack-badge"
+              fontSize={["xs", null, null, null, null, null]}
+              colorScheme="blue"
+            >
+              Python
+            </Badge>
+            <Badge
+              className="stack-badge"
+              fontSize={["xs", null, null, null, null, null]}
+              colorScheme="purple"
+            >
+              Hugging Face
+            </Badge>
+            <Badge
+              className="stack-badge"
+              fontSize={["xs", null, null, null, null, null]}
+              colorScheme="red"
+            >
+              Streamlit
+            </Badge>
+            <Badge
+              className="stack-badge"
+              fontSize={["xs", null, null, null, null, null]}
+              colorScheme="orange"
+            >
+              Prophet
+            </Badge>
+          </Box>
+        </CreationGridItem>
+      </Box>
       <Box>
         <CreationGridItem
           title="Face Recognition System"
@@ -46,61 +84,6 @@ const Creations = () => {
               colorScheme="orange"
             >
               SQLite3
-            </Badge>
-          </Box>
-        </CreationGridItem>
-      </Box>
-      <Box>
-        <CreationGridItem
-          title="Japanese Language Renshū"
-          thumbnail={japaneseLanguageRenshuuThumbnail}
-          link="https://github.com/1391819/japanese-language-renshuu"
-        >
-          <Box pb={1}>
-            <Badge
-              className="stack-badge"
-              fontSize={["xs", null, null, null, null, null]}
-              colorScheme="blue"
-            >
-              Python
-            </Badge>
-          </Box>
-        </CreationGridItem>
-      </Box>
-      <Box>
-        <CreationGridItem
-          title="Tic Tac Toe"
-          thumbnail={generalProjectThumbnail}
-          link="https://github.com/1391819/tic-tac-toe"
-        >
-          <Box pb={1}>
-            <Badge
-              className="stack-badge"
-              fontSize={["xs", null, null, null, null, null]}
-              colorScheme="blue"
-            >
-              C++
-            </Badge>
-            <Badge
-              className="stack-badge"
-              fontSize={["xs", null, null, null, null, null]}
-              colorScheme="purple"
-            >
-              Linux
-            </Badge>
-            <Badge
-              className="stack-badge"
-              fontSize={["xs", null, null, null, null, null]}
-              colorScheme="red"
-            >
-              Minimax
-            </Badge>
-            <Badge
-              className="stack-badge"
-              fontSize={["xs", null, null, null, null, null]}
-              colorScheme="orange"
-            >
-              TCP/IP
             </Badge>
           </Box>
         </CreationGridItem>
@@ -201,6 +184,23 @@ const Creations = () => {
               colorScheme="purple"
             >
               UX/UI
+            </Badge>
+          </Box>
+        </CreationGridItem>
+      </Box>
+      <Box>
+        <CreationGridItem
+          title="Japanese Language Renshū"
+          thumbnail={japaneseLanguageRenshuuThumbnail}
+          link="https://github.com/1391819/japanese-language-renshuu"
+        >
+          <Box pb={1}>
+            <Badge
+              className="stack-badge"
+              fontSize={["xs", null, null, null, null, null]}
+              colorScheme="blue"
+            >
+              Python
             </Badge>
           </Box>
         </CreationGridItem>
