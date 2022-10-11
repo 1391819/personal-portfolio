004655626c92c018b493aa35e8e043102c7e5700
1391819 <roberto.nacu@gmail.com>
2022-10-11 14:23:28 +0100

    removed japanese language renshu project, added tic tac toe

--- components/Creations.js
+++ components/Creations.js
@@ -3,7 +3,6 @@ import CreationGridItem from "./CreationGridItem.js";
 
 // thumbnails
 import faceRecognitionThumbnail from "../public/images/creations/face-recognition/face_recognition_showcase.png";
-import japaneseLanguageRenshuuThumbnail from "../public/images/creations/japanese-language-renshuu/japanese_language_renshuu_showcase.png";
 import interactiveLowPolyEnvironmentThumbnail from "../public/images/creations/interactive-low-poly-environment/interactive_low_poly_environment_showcase.png";
 import maSeekThumbnail from "../public/images/creations/MA-seek/ma_seek_showcase.png";
 import uniSocialMediaThumbnail from "../public/images/creations/social-media-app/uni_social_media_showcase.png";
@@ -156,9 +155,9 @@ const Creations = () => {
       </Box>
       <Box>
         <CreationGridItem
-          title="Japanese Language Renshū"
-          thumbnail={japaneseLanguageRenshuuThumbnail}
-          link="https://github.com/1391819/japanese-language-renshuu"
+          title="Tic Tac Toe"
+          thumbnail={generalProjectThumbnail}
+          link="https://github.com/1391819/tic-tac-toe"
         >
           <Box pb={1}>
             <Badge
@@ -166,7 +165,14 @@ const Creations = () => {
               fontSize={["xs", null, null, null, null, null]}
               colorScheme="blue"
             >
-              CLI
+              TCP/IP
+            </Badge>
+            <Badge
+              className="stack-badge"
+              fontSize={["xs", null, null, null, null, null]}
+              colorScheme="purple"
+            >
+              Minimax
             </Badge>
           </Box>
         </CreationGridItem>
