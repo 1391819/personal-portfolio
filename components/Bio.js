390e9aa87d92168cd79da59cf90937f0a2664823
1391819 <roberto.nacu@gmail.com>
2022-11-23 14:05:04 +0000

    code refactoring

--- components/Bio.js
+++ components/Bio.js
@@ -1,12 +1,12 @@
-import { Box } from "@chakra-ui/react";
-import styled from "@emotion/styled";
+import { Box } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 
 export const BioSection = styled(Box)`
-  display: flex;
-  justify-content: left;
+	display: flex;
+	justify-content: left;
 `;
 
 export const BioYear = styled.span`
-  font-weight: bold;
-  margin-right: 1em;
+	font-weight: bold;
+	margin-right: 1em;
 `;
--- components/voxel-raccoon.js
+++ components/voxel-raccoon.js
@@ -1,131 +1,131 @@
-import { useState, useEffect, useRef, useCallback } from "react";
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { loadGLTFModel } from "../lib/model";
-import { RaccoonSpinner, RaccoonContainer } from "./voxel-raccoon-loader";
+import { useState, useEffect, useRef, useCallback } from 'react';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { loadGLTFModel } from '../lib/model';
+import { RaccoonSpinner, RaccoonContainer } from './voxel-raccoon-loader';
 
 function easeOutCirc(x) {
-  return Math.sqrt(1 - Math.pow(x - 1, 4));
+	return Math.sqrt(1 - Math.pow(x - 1, 4));
 }
 
 const VoxelRaccoon = () => {
-  const refContainer = useRef();
-  const [loading, setLoading] = useState(true);
-  const [renderer, setRenderer] = useState();
-  const [_camera, setCamera] = useState();
-  const [target] = useState(new THREE.Vector3(2, 3, 0));
-  const [initialCameraPosition] = useState(
-    new THREE.Vector3(
-      50 * Math.sin(2 * Math.PI),
-      10,
-      50 * Math.cos(2 * Math.PI)
-    )
-  );
-  const [scene] = useState(new THREE.Scene());
-  const [_controls, setControls] = useState();
+	const refContainer = useRef();
+	const [loading, setLoading] = useState(true);
+	const [renderer, setRenderer] = useState();
+	const [_camera, setCamera] = useState();
+	const [target] = useState(new THREE.Vector3(2, 3, 0));
+	const [initialCameraPosition] = useState(
+		new THREE.Vector3(
+			50 * Math.sin(2 * Math.PI),
+			10,
+			50 * Math.cos(2 * Math.PI)
+		)
+	);
+	const [scene] = useState(new THREE.Scene());
+	const [_controls, setControls] = useState();
 
-  const handleWindowResize = useCallback(() => {
-    const { current: container } = refContainer;
-    if (container && renderer) {
-      const scW = container.clientWidth;
-      const scH = container.clientHeight;
+	const handleWindowResize = useCallback(() => {
+		const { current: container } = refContainer;
+		if (container && renderer) {
+			const scW = container.clientWidth;
+			const scH = container.clientHeight;
 
-      renderer.setSize(scW, scH);
-    }
-  }, [renderer]);
+			renderer.setSize(scW, scH);
+		}
+	}, [renderer]);
 
-  /* eslint-disable react-hooks/exhaustive-deps */
-  useEffect(() => {
-    const { current: container } = refContainer;
-    if (container && !renderer) {
-      const scW = container.clientWidth;
-      const scH = container.clientHeight;
+	/* eslint-disable react-hooks/exhaustive-deps */
+	useEffect(() => {
+		const { current: container } = refContainer;
+		if (container && !renderer) {
+			const scW = container.clientWidth;
+			const scH = container.clientHeight;
 
-      const renderer = new THREE.WebGLRenderer({
-        antialias: true,
-        alpha: true,
-      });
-      renderer.setPixelRatio(window.devicePixelRatio);
-      renderer.setSize(scW, scH);
-      renderer.outputEncoding = THREE.sRGBEncoding;
-      container.appendChild(renderer.domElement);
-      setRenderer(renderer);
+			const renderer = new THREE.WebGLRenderer({
+				antialias: true,
+				alpha: true,
+			});
+			renderer.setPixelRatio(window.devicePixelRatio);
+			renderer.setSize(scW, scH);
+			renderer.outputEncoding = THREE.sRGBEncoding;
+			container.appendChild(renderer.domElement);
+			setRenderer(renderer);
 
-      const scale = scH * 0.005 + 8;
-      const camera = new THREE.OrthographicCamera(
-        -scale,
-        scale,
-        scale,
-        -scale,
-        0.01,
-        50000
-      );
-      camera.position.copy(initialCameraPosition);
-      camera.lookAt(target);
-      setCamera(camera);
+			const scale = scH * 0.005 + 8;
+			const camera = new THREE.OrthographicCamera(
+				-scale,
+				scale,
+				scale,
+				-scale,
+				0.01,
+				50000
+			);
+			camera.position.copy(initialCameraPosition);
+			camera.lookAt(target);
+			setCamera(camera);
 
-      const ambientLight = new THREE.AmbientLight(0xdddddd, 2);
-      scene.add(ambientLight);
+			const ambientLight = new THREE.AmbientLight(0xdddddd, 2);
+			scene.add(ambientLight);
 
-      const controls = new OrbitControls(camera, renderer.domElement);
-      controls.autoRotate = true;
-      //controls.enablePan = false;
-      controls.enableZoom = false;
-      //controls.enableRotate = false;
-      controls.target = target;
-      setControls(controls);
+			const controls = new OrbitControls(camera, renderer.domElement);
+			controls.autoRotate = true;
+			//controls.enablePan = false;
+			controls.enableZoom = false;
+			//controls.enableRotate = false;
+			controls.target = target;
+			setControls(controls);
 
-      loadGLTFModel(scene, "/raccoon.glb", {
-        receiveShadow: false,
-        castShadow: false,
-      }).then(() => {
-        animate();
-        setLoading(false);
-      });
+			loadGLTFModel(scene, '/raccoon.glb', {
+				receiveShadow: false,
+				castShadow: false,
+			}).then(() => {
+				animate();
+				setLoading(false);
+			});
 
-      let req = null;
-      let frame = 0;
-      const animate = () => {
-        req = requestAnimationFrame(animate);
+			let req = null;
+			let frame = 0;
+			const animate = () => {
+				req = requestAnimationFrame(animate);
 
-        frame = frame <= 100 ? frame + 1 : frame;
+				frame = frame <= 100 ? frame + 1 : frame;
 
-        if (frame <= 100) {
-          const p = initialCameraPosition;
-          const rotSpeed = -easeOutCirc(frame / 120) * Math.PI * 10;
+				if (frame <= 100) {
+					const p = initialCameraPosition;
+					const rotSpeed = -easeOutCirc(frame / 120) * Math.PI * 10;
 
-          camera.position.y = 10;
-          camera.position.x =
-            p.x * Math.cos(rotSpeed) + p.z * Math.sin(rotSpeed);
-          camera.position.z =
-            p.z * Math.cos(rotSpeed) - p.x * Math.sin(rotSpeed);
-          camera.lookAt(target);
-        } else {
-          controls.update();
-        }
+					camera.position.y = 10;
+					camera.position.x =
+						p.x * Math.cos(rotSpeed) + p.z * Math.sin(rotSpeed);
+					camera.position.z =
+						p.z * Math.cos(rotSpeed) - p.x * Math.sin(rotSpeed);
+					camera.lookAt(target);
+				} else {
+					controls.update();
+				}
 
-        renderer.render(scene, camera);
-      };
+				renderer.render(scene, camera);
+			};
 
-      return () => {
-        cancelAnimationFrame(req);
-        renderer.dispose();
-      };
-    }
-  }, []);
+			return () => {
+				cancelAnimationFrame(req);
+				renderer.dispose();
+			};
+		}
+	}, []);
 
-  useEffect(() => {
-    window.addEventListener("resize", handleWindowResize, false);
-    return () => {
-      window.removeEventListener("resize", handleWindowResize, false);
-    };
-  }, [renderer, handleWindowResize]);
+	useEffect(() => {
+		window.addEventListener('resize', handleWindowResize, false);
+		return () => {
+			window.removeEventListener('resize', handleWindowResize, false);
+		};
+	}, [renderer, handleWindowResize]);
 
-  return (
-    <RaccoonContainer ref={refContainer}>
-      {loading && <RaccoonSpinner />}
-    </RaccoonContainer>
-  );
+	return (
+		<RaccoonContainer ref={refContainer}>
+			{loading && <RaccoonSpinner />}
+		</RaccoonContainer>
+	);
 };
 
 export default VoxelRaccoon;
