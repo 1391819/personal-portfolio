cd6a6527e6b2b842c590edad855dea11cf66f7c1
1391819 <roberto.nacu@gmail.com>
2024-11-16 12:38:51 +0000

    chore(index): review grammar

--- pages/index.js
+++ pages/index.js
@@ -222,38 +222,40 @@ export default function Home() {
               <Box fontSize={["sm", null, "md", null, null, null]}>
                 <BioSection>
                   <BioYear>1998</BioYear>
-                  <Paragraph>Born in Milan, Italy</Paragraph>
+                  <Paragraph>Born in Milan, Italy.</Paragraph>
                 </BioSection>
                 <BioSection>
                   <BioYear>2016</BioYear>
                   <Paragraph>
                     Completed studies at P. Paleocapa in Bergamo and earned a
-                    Diploma di Stato in Computer Science
+                    Diploma di Stato in Computer Science.
                   </Paragraph>
                 </BioSection>
                 <BioSection>
                   <BioYear>2017</BioYear>
                   <Paragraph>
-                    Moved to Newcastle upon Tyne, United Kingdom
+                    Moved to Newcastle upon Tyne, United Kingdom.
                   </Paragraph>
                 </BioSection>
                 <BioSection>
                   <BioYear>2020</BioYear>
                   <Paragraph>
                     Studied at Nagoya University of Foreign Studies in Japan,
-                    fullfulling one of my long-held dreams
+                    fulfulling one of my long-held dreams.
                   </Paragraph>
                 </BioSection>
                 <BioSection>
                   <BioYear>2022</BioYear>
                   <Paragraph>
                     Graduated from Northumbria University with a First Class
-                    Honours in Computer Science with AI
+                    Honours in Computer Science with AI.
                   </Paragraph>
                 </BioSection>
                 <BioSection>
                   <BioYear>2023</BioYear>
-                  <Paragraph>Began my journey as a Software Engineer</Paragraph>
+                  <Paragraph>
+                    Began my journey as a Software Engineer.
+                  </Paragraph>
                 </BioSection>
               </Box>
             </Box>
@@ -414,8 +416,8 @@ export default function Home() {
                     />
                     <Paragraph>
                       Completed an intensive 3-month training program, gaining
-                      experience in Python, Git, Docker, Terraform, AWS, Linux
-                      and CI/CD principles
+                      experience in Python, Git, Docker, Terraform, AWS, Linux,
+                      and CI/CD principles.
                     </Paragraph>
                   </ListItem>
                   <ListItem display="flex" alignItems="center">
@@ -442,7 +444,7 @@ export default function Home() {
                       >
                         group-based
                       </Link>{" "}
-                      projects
+                      projects.
                     </Paragraph>
                   </ListItem>
                 </List>
@@ -474,7 +476,7 @@ export default function Home() {
                       Provided remote technical support for CNC software,
                       addressing toolpath optimisation, calibration, and G-code
                       interpretation, achieving a 100% satisfaction rate with
-                      domestic and international clients
+                      domestic and international clients.
                     </Paragraph>
                   </ListItem>
                 </List>
